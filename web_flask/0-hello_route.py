ea0612b33b73f890ec40ba28058e181174c8c555
bigBadMatt <5707@holbertonstudents.com>
2023-04-04 16:49:17 -0700

    addedcheck for __main__ to run on port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,12 +4,12 @@ Returns string upon request
 """
 from flask import Flask, request
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello HBNB"
-
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
